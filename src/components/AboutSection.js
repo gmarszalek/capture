0f1a4c1035caf7df93f4f6302843d2e05bd84d28
gmarszalek <grzegorzmarszalek92@gmail.com>
2021-01-17 21:30:08 +0100

    Added React Project Files

---
+++ src/components/AboutSection.js
@@ -0,0 +1,40 @@
+import home1 from "../img/home1.png";
+
+//Styled
+import { About, Description, Image, Hide } from "../styles";
+
+//Framer Motion
+import { motion } from "framer-motion";
+import { titleAnimation, fade, photoAnimation } from "../animation";
+import Wave from "./Wave";
+const AboutSection = () => {
+  return (
+    <About>
+      <Description>
+        <motion.div>
+          <Hide>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnimation}>
+              your <span>dreams</span> come
+            </motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnimation}>true</motion.h2>
+          </Hide>
+        </motion.div>
+        <motion.p variants={fade}>
+          Contact us for any photography or video ideas that you have. We have professionals with amazing skills.
+        </motion.p>
+        <motion.button variants={fade}>Contact us</motion.button>
+      </Description>
+      <Image>
+        <motion.img variants={photoAnimation} src={home1} alt="guy with a camera" />
+      </Image>
+      <Wave />
+    </About>
+  );
+};
+
+export default AboutSection;
---
+++ src/components/Nav.js
@@ -0,0 +1,102 @@
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
+
+import styled from "styled-components";
+
+const Nav = () => {
+  const { pathname } = useLocation();
+  return (
+    <StyledNav>
+      <h1>
+        <Link id="logo" to="/">
+          Capture
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/capture">1. About Us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/" ? "50%" : "0%" }}
+          />
+        </li>
+        <li>
+          <Link to="/capture/work">2. Our Work</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/work" ? "50%" : "0%" }}
+          />
+        </li>
+        <li>
+          <Link to="/capture/contact">3. Contact Us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/contact" ? "50%" : "0%" }}
+          />
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background: #282828;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+  #logo {
+    font-size: 1.5rem;
+    font-family: "Lobster", cursive;
+    font-weight: lighter;
+  }
+  @media (max-width: 1300px) {
+    flex-direction: column;
+    padding: 1rem;
+    #logo {
+      display: inline-block;
+      margin: 1rem;
+    }
+    ul {
+      padding: 2rem;
+      justify-content: space-around;
+      width: 100%;
+      li {
+        padding: 0;
+      }
+    }
+  }
+`;
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 0%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  @media (max-width: 1300px) {
+    left: 0%;
+  }
+`;
+export default Nav;
